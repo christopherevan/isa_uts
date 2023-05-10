54e3e9bd6724380b06f3e014fa6023abcf98ca58
anellaaaa <anellanevanyatjokro@gmail.com>
2023-05-10 16:32:24 +0700

    minor komet

--- cek_account.php
+++ cek_account.php
@@ -11,26 +11,42 @@ if (isset($_POST['acc_id'])) {
     $enc_session = $aes->encrypt($_SESSION['username']);
     $enc_aid = $aes->encrypt($acc_id);
 
-    // lanjut cek apakh yg ditransferi sama apa ga
-    $sql = "SELECT account_id FROM accounts WHERE account_id=?";
+    $sql = "SELECT a.account_id FROM users as u INNER JOIN customers as c ON u.username=c.username 
+    INNER JOIN accounts as a ON c.customer_id=a.customer_id WHERE u.username=?";
     $stmt = $conn->mysqli->prepare($sql);
-    $stmt->bind_param('s', $enc_aid);
+    $stmt->bind_param('s', $enc_session);
     $stmt->execute();
     $res = $stmt->get_result();
-    $nrows = $res->num_rows;
-
-    if ($nrows > 0) {
-        $sql = "SELECT c.name FROM accounts AS a INNER JOIN customers AS c ON c.customer_id=a.customer_id WHERE a.account_id=?";
+    
+    while ($row = $res->fetch_assoc()){
+        $sender_acc_id = $row['account_id'];
+    }
+    
+    if(($sender_acc_id==$enc_aid)){
+        echo 'same';
+    }else{
+        $sql = "SELECT account_id FROM accounts WHERE account_id=?";
         $stmt = $conn->mysqli->prepare($sql);
         $stmt->bind_param('s', $enc_aid);
         $stmt->execute();
         $res = $stmt->get_result();
-        while ($row = $res->fetch_assoc()) {
-            echo $aes->decrypt($row['name']);
+        $nrows = $res->num_rows;
+
+        if ($nrows > 0) {
+            $sql = "SELECT c.name FROM accounts AS a INNER JOIN customers AS c ON c.customer_id=a.customer_id WHERE a.account_id=?";
+            $stmt = $conn->mysqli->prepare($sql);
+            $stmt->bind_param('s', $enc_aid);
+            $stmt->execute();
+            $res = $stmt->get_result();
+            while ($row = $res->fetch_assoc()) {
+                echo $aes->decrypt($row['name']);
+            }
+        } else {
+            echo 'false';
         }
-    } else {
-        echo 'false';
     }
+    // lanjut cek apakh yg ditransferi sama apa ga
+    
 
     $conn->mysqli->close();
 }
--- change_password.php
+++ change_password.php
@@ -6,25 +6,17 @@ session_start();
 $aes = new AES();
 $conn = new conn();
 
-// $enc_user = $aes->encrypt($_SESSION['username']);
-// $sql = "SELECT u.*, c.*, a.* FROM users as u INNER JOIN customers as c ON u.username=c.username 
-// INNER JOIN accounts as a ON c.customer_id=a.customer_id WHERE u.username=?";
-// $stmt = $conn->mysqli->prepare($sql);
-// $stmt->bind_param('s', $enc_user);
-// $stmt->execute();
-// $res = $stmt->get_result();
+$enc_user = $aes->encrypt($_SESSION['username']);
+$sql = "SELECT c.name FROM users as u INNER JOIN customers as c ON u.username=c.username WHERE u.username=?";
+$stmt = $conn->mysqli->prepare($sql);
+$stmt->bind_param('s', $enc_user);
+$stmt->execute();
+$res = $stmt->get_result();
 
-// if ($row = $res->fetch_assoc()){
-//     $user = array($row['name'], $row['email'], $row['phone_number'],  $row['address'], $row['account_id']);
-//     $user_arr = array('name', 'email', 'tel', 'address', 'accountnum');
-//     $dec_user = array();
-// }
-
-// $i = 0;
-// foreach ($user as $u) {
-//     $dec_user[$user_arr[$i]] = $aes->decrypt($u);
-//     $i++;
-// }
+if ($row = $res->fetch_assoc()){
+    $user = $row['name'];
+}
+$dec_user = $aes->decrypt($user);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +29,7 @@ $conn = new conn();
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Mi-Bank</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -88,13 +80,13 @@ $conn = new conn();
             </div>
             <li class="nav-item">
                 <a class="nav-link" href="transfer.php">
-                    <i class="fas fa-fw fa-cog"></i>
+                    <i class="fas fa-fw fa-paper-plane"></i>
                     <span>Transfer</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="transaction_history.php">
-                    <i class="fas fa-fw fa-wrench"></i>
+                    <i class="fas fa-fw fa-history"></i>
                     <span>History</span>
                 </a>
             </li>
@@ -104,13 +96,13 @@ $conn = new conn();
             </div>
             <li class="nav-item active">
                 <a class="nav-link" href="user_profile.php" >
-                    <i class="fas fa-fw fa-folder"></i>
+                    <i class="fas fa-fw fa-user"></i>
                     <span>My Account</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="logout.php">
-                    <i class="fas fa-fw fa-chart-area"></i>
+                    <i class="fas fa-fw fa-sign-out-alt"></i>
                     <span>Logout</span></a>
             </li>
 
@@ -172,129 +164,18 @@ $conn = new conn();
                         </li>
 
                         <!-- Nav Item - Alerts -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-bell fa-fw"></i>
-                                <!-- Counter - Alerts -->
-                                <span class="badge badge-danger badge-counter">3+</span>
-                            </a>
-                            <!-- Dropdown - Alerts -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="alertsDropdown">
-                                <h6 class="dropdown-header">
-                                    Alerts Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-primary">
-                                            <i class="fas fa-file-alt text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-success">
-                                            <i class="fas fa-donate text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-exclamation-triangle text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                            </div>
-                        </li>
+                        
 
                         <!-- Nav Item - Messages -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-                            <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-envelope fa-fw"></i>
-                                <!-- Counter - Messages -->
-                                <span class="badge badge-danger badge-counter">7</span>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="messagesDropdown">
-                                <h6 class="dropdown-header">
-                                    Message Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_1.svg"
-                                            alt="...">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div class="font-weight-bold">
-                                        <div class="text-truncate">Hi there! I am wondering if you can help me with a
-                                            problem I've been having.</div>
-                                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_2.svg"
-                                            alt="...">
-                                        <div class="status-indicator"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">I have the photos that you ordered last month, how
-                                            would you like them sent to you?</div>
-                                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_3.svg"
-                                            alt="...">
-                                        <div class="status-indicator bg-warning"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Last month's report looks great, I am very happy with
-                                            the progress so far, keep up the good work!</div>
-                                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
-                                            alt="...">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Am I a good boy? The reason I ask is because someone
-                                            told me that people say this to all dogs, even if they aren't good...</div>
-                                        <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-                            </div>
-                        </li>
+                        
 
                         <div class="topbar-divider d-none d-sm-block"></div>
 
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                            <a class="nav-link dropdown-toggle" href="user_profile.php" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $_SESSION['username'];?></span>
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $dec_user;?></span>
                                 <img class="img-profile rounded-circle"
                                     src="img/undraw_profile.svg">
                             </a>
@@ -390,13 +271,7 @@ $conn = new conn();
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
+            
             <!-- End of Footer -->
 
         </div>
